651f67c0dd2df2587fcdb70aa539cf372b780697
Venkataramana <yegireddi.venkataramana@gmail.com>
2020-10-22 18:15:25 +0530

    added code for hooks and taggedhooks

--- src/main/java/myRunner/TestRunner.java
+++ src/main/java/myRunner/TestRunner.java
@@ -10,13 +10,13 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		
 		features = "D:\\Selenium Jars\\Selenium Projects\\LocalBadabing Project with Selenium\\Badabing Project"
-				+ "\\FreeCrmBDDFramework\\src\\main\\java\\Features\\tagging.feature", //the path of the feature files
+				+ "\\FreeCrmBDDFramework\\src\\main\\java\\Features\\taggedhooks.feature", //the path of the feature files
 		glue = {"stepDefinitions"}, //the path of the step definition files
 		format={"pretty", "html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
 		monochrome = true, //display the console output in a proper readable format
 		strict = true, //it will check if any step is not defined in step definition file
-		dryRun = false, //to check the mapping is proper between feature file and step definition file
-		tags = {"~@SmokeTest" , "~@RegressionTest" , "~@End2End"}
+		dryRun = false //to check the mapping is proper between feature file and step definition file
+		//tags = {"~@SmokeTest" , "~@RegressionTest" , "~@End2End"}
 		)
 
 
