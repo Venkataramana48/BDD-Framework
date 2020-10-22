66555d28b9c3a7e1cbb151c13178659c775db8cf
Venkataramana <yegireddi.venkataramana@gmail.com>
2020-10-22 16:04:49 +0530

    added tags feature in cucumber

--- src/main/java/myRunner/TestRunner.java
+++ src/main/java/myRunner/TestRunner.java
@@ -10,12 +10,13 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		
 		features = "D:\\Selenium Jars\\Selenium Projects\\LocalBadabing Project with Selenium\\Badabing Project"
-				+ "\\FreeCrmBDDFramework\\src\\main\\java\\Features\\dealsmap.feature", //the path of the feature files
+				+ "\\FreeCrmBDDFramework\\src\\main\\java\\Features\\tagging.feature", //the path of the feature files
 		glue = {"stepDefinitions"}, //the path of the step definition files
 		format={"pretty", "html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
 		monochrome = true, //display the console output in a proper readable format
 		strict = true, //it will check if any step is not defined in step definition file
-		dryRun = false //to check the mapping is proper between feature file and step definition file
+		dryRun = false, //to check the mapping is proper between feature file and step definition file
+		tags = {"~@SmokeTest" , "~@RegressionTest" , "~@End2End"}
 		)
 
 
@@ -23,3 +24,6 @@ import cucumber.api.junit.Cucumber;
 public class TestRunner {
 
 }
+
+//ORed : tags = {"@SmokeTest , @RegressionTest"} -- execute all tests tagged as @SmokeTest OR @RegressionTest
+	//ANDed : tags = tags = {"@SmokeTest" , "@RegressionTest"} -- execute all tests tagged as @SmokeTest AND @RegressionTest
--- test-output/report.js
+++ test-output/report.js
@@ -1,167 +1,35 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/Selenium Jars/Selenium Projects/LocalBadabing Project with Selenium/Badabing Project/FreeCrmBDDFramework/src/main/java/Features/dealsmap.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/Selenium Jars/Selenium Projects/LocalBadabing Project with Selenium/Badabing Project/FreeCrmBDDFramework/src/main/java/Features/tagging.feature");
 formatter.feature({
-  "line": 1,
-  "name": "Free CRM Create Deals",
+  "line": 2,
+  "name": "Free CRM application testing",
   "description": "",
-  "id": "free-crm-create-deals",
-  "keyword": "Feature"
+  "id": "free-crm-application-testing",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@FunctionalTest"
+    }
+  ]
 });
 formatter.scenario({
-  "line": 3,
-  "name": "Free CRM Create a new Deal Scenario",
+  "line": 72,
+  "name": "browser closed",
   "description": "",
-  "id": "free-crm-create-deals;free-crm-create-a-new-deal-scenario",
+  "id": "free-crm-application-testing;browser-closed",
   "type": "scenario",
   "keyword": "Scenario"
 });
 formatter.step({
-  "line": 5,
-  "name": "user is already login page",
+  "line": 73,
+  "name": "This is a close broswer test",
   "keyword": "Given "
 });
-formatter.step({
-  "line": 6,
-  "name": "title of login page is Free CRM",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 7,
-  "name": "user enters username and password",
-  "rows": [
-    {
-      "cells": [
-        "username",
-        "password"
-      ],
-      "line": 8
-    },
-    {
-      "cells": [
-        "yegireddi.venkataramana@gmail.com",
-        "Mangamma!#1990"
-      ],
-      "line": 9
-    }
-  ],
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 11,
-  "name": "user clicks on login button",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 12,
-  "name": "user is on home page",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 13,
-  "name": "user moves to new deal page",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 14,
-  "name": "user enters deal details",
-  "rows": [
-    {
-      "cells": [
-        "title",
-        "probability",
-        "amount",
-        "commission"
-      ],
-      "line": 15
-    },
-    {
-      "cells": [
-        "test deal1",
-        "60",
-        "2000",
-        "20"
-      ],
-      "line": 16
-    },
-    {
-      "cells": [
-        "test deal2",
-        "70",
-        "3000",
-        "30"
-      ],
-      "line": 17
-    },
-    {
-      "cells": [
-        "test deal3",
-        "80",
-        "4000",
-        "40"
-      ],
-      "line": 18
-    }
-  ],
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 20,
-  "name": "close the browser",
-  "keyword": "Then "
-});
 formatter.match({
-  "location": "DealsStepWithMapDefinition.user_is_already_login_page()"
+  "location": "TaggingStepDefinition.this_is_a_close_broswer_test()"
 });
 formatter.result({
-  "duration": 16459235500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.title_of_login_page_is_Free_CRM()"
-});
-formatter.result({
-  "duration": 20086200,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.user_enters_username_and_password(DataTable)"
-});
-formatter.result({
-  "duration": 764740100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.user_clicks_on_login_button()"
-});
-formatter.result({
-  "duration": 5202142400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.user_is_on_home_page()"
-});
-formatter.result({
-  "duration": 10031700,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.user_moves_to_new_deal_page()"
-});
-formatter.result({
-  "duration": 3641567100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.user_enters_deal_details(DataTable)"
-});
-formatter.result({
-  "duration": 37984587400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "DealsStepWithMapDefinition.close_the_browser()"
-});
-formatter.result({
-  "duration": 5785755200,
+  "duration": 158458300,
   "status": "passed"
 });
 });
